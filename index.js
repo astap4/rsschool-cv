9663dee4e4320804b019d5029c888d3cf368e4b7
Alina <astapchik.1993@mail.ru>
2023-05-29 00:22:58 +0300

    feat: remove webpack

--- index.js
+++ index.js
@@ -1,51 +1,42 @@
-import '../style.scss';
-import js_image from './assets/avatar.jpg';
-import js_project1 from './assets/travel.png';
-import js_project2 from './assets/shelter.png';
-import js_project3 from './assets/virtual-keyboard.png';
-import js_project4 from './assets/minesweeper.png';
-import js_project5 from './assets/online-store.png';
-import js_project6 from './assets/puzzle.png';
-
 const projects = [
   {
     title: 'Travel',
-    imageSrc: js_project1,
+    imageSrc: './assets/travel.png',
     url: 'https://rolling-scopes-school.github.io/astap4-JSFEPRESCHOOL2022Q2/Travel/'
   },
   {
     title: 'Shelter',
-    imageSrc: js_project2,
+    imageSrc: './assets/shelter.png',
     url: 'https://rolling-scopes-school.github.io/astap4-JSFE2023Q1/Shelter/'
   },
   {
     title: 'Virtual-keyboard',
-    imageSrc: js_project3,
+    imageSrc: './assets/virtual-keyboard.png',
     url: 'https://astap4.github.io/virtual-keyboard/'
   },
   {
     title: 'Minesweeper',
-    imageSrc: js_project4,
+    imageSrc: './assets/minesweeper.png',
     url: 'https://rolling-scopes-school.github.io/astap4-JSFE2023Q1/minesweeper/'
   },
   {
     title: 'Online-shop',
-    imageSrc: js_project5,
+    imageSrc: './assets/online-store.png',
     url: 'https://astap4-siarheiliavontsyeu-onlinestore.netlify.app/#main'
   },
   {
     title: 'Puzzle',
-    imageSrc: js_project6,
+    imageSrc: './assets/puzzle.png',
     url: 'https://rolling-scopes-school.github.io/astap4-JSFE2022Q3/puzzle/'
   },
 ]
 
-const imageContainer = document.querySelector('.my-photo')
-const myImage = document.createElement('img');
-myImage.classList.add('my-photo-img')
-myImage.src = js_image;
+// const imageContainer = document.querySelector('.my-photo')
+// const myImage = document.createElement('img');
+// myImage.classList.add('my-photo-img')
+// myImage.src = js_image;
 
-imageContainer.append(myImage);
+// imageContainer.append(myImage);
 
 const projectsContainer = document.querySelector('.projects')
 
